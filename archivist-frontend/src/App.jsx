5c2cb01b3bda8ec056c01793c584753407487b2f
Naresh B A <naresh.b@prodapt.com>
2025-10-07 12:00:33 +0530

    frontend added

---
+++ archivist-frontend/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import { Container, Button } from 'react-bootstrap';
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <Container className="text-center bg-black">
+    </Container>
+  )
+}
+
+export default App
